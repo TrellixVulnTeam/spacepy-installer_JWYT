bf81f8339aa854b1ee19ed88e4d2c0c416e57754
Michael Hirsch <scivision@users.noreply.github.com>
2020-11-08 14:39:18 -0500

    workaround spacepy/setup.py prereqs issue

--- setup_spacepy.py
+++ setup_spacepy.py
@@ -74,7 +74,9 @@ def setup_spacepy():
     subprocess.check_call(build_cmd)
     subprocess.check_call([make, "-C", str(cwd), "--silent", "install"])  # no sudo
     # %% install spacepy
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy", "spacepy"])
+    # numpy must be installed separately, as spacepy/setup.py isn't handling this as usual
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "spacepy"])
     # %% verify
     print("verifying SpacePy CDF read")
 
